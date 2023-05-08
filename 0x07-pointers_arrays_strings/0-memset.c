ebe0da1da1d3e80652cb405df6a58b2545ffa49d
Doxa2023 <Doxa2023>
2023-05-08 14:47:06 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _memset - fill a block of memoty with a specific value
  * @s: starting address memory to be filled
@@ -11,6 +10,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
+
 	for (; n > 0; i++)
 	{
 		s[i] = b;
